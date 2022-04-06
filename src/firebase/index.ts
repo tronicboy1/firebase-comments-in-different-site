ebb24d6122a7b2951b755a0b69350eaba11cc1ae
Austin Mayer <a.inoue@churadata.okinawa>
2022-04-06 17:27:48 +0900

    add firebase config

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -3,13 +3,14 @@ import { getDatabase } from "firebase/database";
 
 // 必須 : Firebase Consoleで取得した認証情報をここに入れる
 const firebaseConfig = {
-  apiKey: "AIzaSyC2nuA1NE2iZc5LXW9D36e0B1bIlJ6FVVI",
-  authDomain: "realtime-comments-22c1f.firebaseapp.com",
-  databaseURL: "https://realtime-comments-22c1f-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "realtime-comments-22c1f",
-  storageBucket: "realtime-comments-22c1f.appspot.com",
-  messagingSenderId: "220482101835",
-  appId: "1:220482101835:web:f83c86e46be084802923ce"
+  apiKey: "API_KEY",
+  authDomain: "PROJECT_ID.firebaseapp.com",
+  databaseURL: "https://DATABASE_NAME.firebaseio.com",
+  projectId: "PROJECT_ID",
+  storageBucket: "PROJECT_ID.appspot.com",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID",
+  measurementId: "G-MEASUREMENT_ID",
 };
 
 const app = initializeApp(firebaseConfig);
