2707679f88667d729388bcda31040ff9c27ac83a
Austin Mayer <a.inoue@churadata.okinawa>
2022-04-06 15:46:14 +0900

    add css loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,26 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.tsx',
+  entry: "./src/index.tsx",
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
   mode: "production",
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: [".tsx", ".ts", ".js"],
   },
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public/js'),
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public/js"),
   },
 };
