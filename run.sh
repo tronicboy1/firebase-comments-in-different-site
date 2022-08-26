4349479bfc72e6e0e9c547debde08e6e96c65899
Austin Mayer <95405354+tronicboy1@users.noreply.github.com>
2022-08-26 18:42:13 +0900

    fix command

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-bundle exec rake db:migrate
-bundle exec rails server
-rails server -e production -b 0.0.0.0
+# bundle exec rake db:migrate
+bundle exec rails server -e production -b 0.0.0.0
